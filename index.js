7c4906209121a08011c40adf285830a2959f0f8c
Dominic Misasi <meshtatsuo@gmail.com>
2022-02-27 10:35:21 -0700

    some comments on index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+/* ------------------------------------------------------------------------------------------
+    TEAM PROFILE GENERATOR | Generate a webpage for your team based on user input
+    Author: Dominic Misasi
+   ------------------------------------------------------------------------------------------ */
+
+// stuff here
